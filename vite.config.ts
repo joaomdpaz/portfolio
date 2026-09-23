ff945ce2e465cd4903578ac6a6939923a1dcff72
João Paz <jpaz@cuco.net>
2026-09-23 12:45:28 +0100

    config git pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,13 @@ export default defineConfig({
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
-			adapter: adapter()
+			paths: {
+				base: process.argv.includes('dev') ? '' : '/portolio'
+			},
+			adapter: adapter({
+				fallback: '404.html'
+				
+			})
 		})
 	]
 });
